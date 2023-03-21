30461841cd505bb780206506d97c178d2ee1be15
diego-cavenati <diegocavenati@gmail.com>
2023-03-21 11:41:28 +0100

    add responsivity to footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,29 @@
 <script>
 export default {
     name: 'FooterComponent',
+    data() {
+        return {
+            isWide: false,
+        };
+    },
+    mounted() {
+        this.isWide = window.innerWidth >= 390;
+        window.addEventListener('resize', this.onResize);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        onResize() {
+            this.isWide = window.innerWidth >= 391;
+        },
+    },
 }
 </script>
 
 <template>
     <footer>
-        <div class="container d-flex">
+        <div class="container" :class="{ 'd-flex': isWide }">
             <div class="links">
                 <ul>
                     <li><a href="#">Il Gruppo</a></li>
@@ -27,6 +44,8 @@ export default {
 
 <style lang="scss">
 @use '../styles/general.scss';
+@use '../styles/partials/variables.scss' as *;
+
 
 footer {
     padding: 3rem 0;
@@ -43,5 +62,40 @@ footer {
         margin-left: auto;
     }
 
+}
+
+/**********************
+      Responsive
+***********************/
+@media screen and (max-width: 390px) {
+    footer {
+        padding: 3rem 0;
+
+        ul {
+            display: block;
+
+            li {
+                padding-right: 0;
+                padding-bottom: 1rem;
+                font-family: $font-primary;
+                font-size: 16px;
+                font-weight: 700;
+                line-height: 24px;
+                letter-spacing: 0em;
+                text-align: left;
+
+                &:not(:last-child) {
+                    padding-bottom: 1rem;
+                }
+            }
+
+
+        }
+
+        .copyright {
+            margin-right: auto;
+        }
+
+    }
 }
 </style>
